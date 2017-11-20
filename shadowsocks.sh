648091b2a7fd38729fedcf2a91bc0889ef76a5f6
随心 <1023293003@qq.com>
2017-11-20 13:57:20 +0800

    ss脚本修改

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,19 +1,14 @@
-{
- "server":"148.251.159.88",
- "local_address": "127.0.0.1",
- "local_port":1080,
-  "port_password": {
-     "8391": "8391",
-     "8392": "8392",
-     "8393": "8393",
-     "8394": "8394"
- },
- "timeout":300,
- "method":"aes-256-cfb",
- "fast_open": false
-}
+#!/bin/sh
+echo "安装pip"
+sudo yum install python-setuptools && easy_install pip
 
+echo "使用pip安装shadowsocks"
+sudo pip install shadowsocks
 
+echo "删除已有配置文件"
+rm -rf /etc/shadowsocks.json
+
+echo "创建并写入配置文件"
 echo "{
     \"server\":\"0.0.0.0\",
     \"local_address\": \"127.0.0.1\",
@@ -28,3 +23,6 @@ echo "{
     \"method\":\"aes-256-cfb\",
     \"fast_open\": false
 }" >>/etc/shadowsocks.json
+
+echo "启动shadowsocks"
+ssserver -c /etc/shadowsocks.json -d start
